48e9234b87284fb7e144a969da145af24298e8c4
krxsky <krx.sky@gmail.com>
2014-06-06 17:51:59 -0700

    add in app to load old data and project structure
    Change-Id: I97b14822398fe8cd4439d1e6e2c188297f3c15a2

---
+++ generator/src/ca/ubc/cs/commandrecommender/data/injector/CsvUtils.java
@@ -0,0 +1,81 @@
+package ca.ubc.cs.commandrecommender.data.injector;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * Created by KeEr on 2014-06-06.
+ */
+public class CsvUtils {
+
+    /**
+     * Split the String parameter into substrings. The parameter is assumed to
+     * be in CSV format. Comma separators are assumed. An entry that starts and
+     * ends with double-quotes may contain commas or escaped double-quotes.
+     * Double-quotes are escaped by putting two one-after-the-other. This method
+     * assumes that there are no extraneous white spaces (i.e. leading or
+     * trailing) in the input.
+     * <p>
+     * Note that we don't worry about trying to re-translate escaped characters
+     * back into their unescaped form. We assume that this method is used exclusively
+     * for displaying events in a preview pane, or for applying filters; we don't
+     * need to translate the escaped characters in either of these cases.
+     * <p>
+     * The value: "first,\"\"\"second\"\", third\",fourth" will be parsed into
+     * three strings: "first", "\"second\", third", and "fourth".
+     * <p>
+     * Note that callers can safely assume that all entries in the resulting
+     * array will be non-<code>null</code>.
+     *
+     * @param line
+     *            a {@link String}. Must not be <code>null</code>.
+     *
+     * @return an array of {@link String}s.
+     */
+    public static String[] splitLine(String line) {
+        List<String> strings = new java.util.ArrayList<String>();
+        try {
+            splitLine(line, strings);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return (String[]) strings.toArray(new String[strings.size()]);
+    }
+
+    private static void splitLine(String line, List<String> strings) throws IOException {
+		/*
+		 * There is a potential issue with this implementation in the case
+		 * where a quoted-wrapped field starts with an escaped quote. i.e.
+		 * the string \"\"\"value\"\"\" will be read as escaped-quote, followed
+		 * by quote rather than as quote followed by escaped-quote as is
+		 * intended. The net result is the same (evidenced in the test cases).
+		 */
+        Reader reader = new StringReader(line);
+        int next;
+        StringBuilder builder = new StringBuilder();
+        boolean inQuote = false;
+        while ((next = reader.read()) != -1) {
+            if (next == '"') {
+                reader.mark(1);
+                if (reader.read() == '"') {
+                    builder.append('"');
+                } else {
+                    reader.reset();
+                    inQuote = !inQuote;
+                }
+            } else if (next == ',') {
+                if (inQuote) {
+                    builder.append(',');
+                } else {
+                    strings.add(builder.toString());
+                    builder = new StringBuilder();
+                }
+
+            } else {
+                builder.append((char)next);
+            }
+        }
+        strings.add(builder.toString());
+    }
+}
---
+++ generator/src/ca/ubc/cs/commandrecommender/data/injector/OldUsageDataInjector.java
@@ -0,0 +1,70 @@
+package ca.ubc.cs.commandrecommender.data.injector;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+import java.io.*;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by KeEr on 2014-06-06.
+ */
+public class OldUsageDataInjector {
+
+    public static final String DB_NAME = "commands-development";
+    public static final String COMMANDS_COLLECTION = "commands";
+
+    private static final String USER_ID = "user_id";
+    private static final String WHAT = "what";
+    private static final String KIND = "kind";
+    private static final String BUNDLE_VERSION = "bundleVersion";
+    private static final String TIME = "time";
+    private static final String DESCRIPTION = "description";
+    private static final String BINDING_USED = "bindingUsed";
+
+    public static void main(String args[]) throws IOException {
+        String filePath = args[0];
+        //establish connection
+        MongoClient mongoClient = new MongoClient();
+        DBCollection collection = mongoClient.getDB(DB_NAME)
+                .getCollection(COMMANDS_COLLECTION);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                new FileInputStream(filePath)));
+        try {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                insertCommand(collection, line);
+            }
+        } finally {
+            reader.close();
+        }
+    }
+
+    private static void insertCommand(DBCollection collection, String line) {
+        String[] tokens = CsvUtils.splitLine(line);
+        if (tokens == null)
+            return;
+        Long when;
+        try {
+            when = Long.valueOf(tokens[6].trim());
+        } catch (NumberFormatException e) {
+            return;
+        }
+        DBObject entry = new BasicDBObject();
+        entry.put(USER_ID, tokens[0]);
+        entry.put(WHAT, tokens[1]);
+        entry.put(KIND, tokens[2]);
+        entry.put(BUNDLE_VERSION, tokens[4]);
+        entry.put(DESCRIPTION, tokens[5]);
+        entry.put(TIME, when);
+        entry.put(BINDING_USED, true);
+        collection.insert(entry);
+    }
+
+}
