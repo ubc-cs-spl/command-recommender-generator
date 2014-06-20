44e36acf490933fcb9f253e7dbc3473ae61757ac
krxsky <krx.sky@gmail.com>
2014-06-19 17:05:12 -0700

    added in guava, apache and ToolUse ToolUses
    Change-Id: I925f19c9301f71d589b35f18bb016482785e6eb0

---
+++ generator/src/main/java/ca/ubc/cs/commandrecommender/model/ToolUse.java
@@ -0,0 +1,21 @@
+package ca.ubc.cs.commandrecommender.model;
+
+import java.sql.Timestamp;
+
+public class ToolUse {
+
+    public Timestamp time;
+    public int tool;
+    public boolean hotkey;
+
+    public ToolUse(Timestamp time, int tool, boolean hotkey) {
+        this.time = time;
+        this.tool = tool;
+        this.hotkey = hotkey;
+    }
+
+    public String toString(){
+        return Integer.toString(tool);
+    }
+
+}
