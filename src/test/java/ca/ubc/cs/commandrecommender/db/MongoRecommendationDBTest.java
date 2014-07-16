8d09b023b164061a1e84c6d323219d5f7e1ad901
krxsky <krx.sky@gmail.com>
2014-07-16 13:22:27 -0700

    update tests for recommendation db
    Change-Id: I30b9d26253c1b465b77b6ecaef150b6b1d28fb13

--- src/test/java/ca/ubc/cs/commandrecommender/db/MongoRecommendationDBTest.java
+++ src/test/java/ca/ubc/cs/commandrecommender/db/MongoRecommendationDBTest.java
@@ -1,23 +1,24 @@
 package ca.ubc.cs.commandrecommender.db;
 
-import ca.ubc.cs.commandrecommender.Exception.DBConnectionException;
-import ca.ubc.cs.commandrecommender.mocks.MockRecommendationDB;
-import ca.ubc.cs.commandrecommender.model.IndexMap;
-import ca.ubc.cs.commandrecommender.model.Rationale;
-import ca.ubc.cs.commandrecommender.model.User;
-import com.mongodb.*;
+import static org.junit.Assert.assertEquals;
+
+import java.net.UnknownHostException;
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.net.UnknownHostException;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
+import ca.ubc.cs.commandrecommender.Exception.DBConnectionException;
+import ca.ubc.cs.commandrecommender.model.IndexMap;
+import ca.ubc.cs.commandrecommender.model.Rationale;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 /**
  * Created by Spencer on 6/23/2014.
@@ -34,6 +35,7 @@ public class MongoRecommendationDBTest {
     private AbstractRecommendationDB recommendationDB;
     private IndexMap userIndexMap, toolIndexMap;
     private String COMMAND_ID1 = "NEW_COMMAND1";
+    private String COMMAND_ID2 = "NEW_COMMAND2";
     private String USER_ID1 = "NEW_USER1";
     private String USER_ID = "NEW_USER";
     private String REASON1 = "REASON1";
@@ -57,9 +59,11 @@ public class MongoRecommendationDBTest {
     }
 
     private void initializeDataBase() {
-        BasicDBObject commandDetail = new BasicDBObject(MongoRecommendationDB.COMMAND_ID_FIELD, COMMAND_ID1);
-        commandDetailsCollection.insert(commandDetail);
-        command_detail_object_id_1 = (ObjectId)commandDetail.get("_id");
+        BasicDBObject commandDetail1 = new BasicDBObject(MongoRecommendationDB.COMMAND_ID_FIELD, COMMAND_ID1);
+        BasicDBObject commandDetail2 = new BasicDBObject(MongoRecommendationDB.COMMAND_ID_FIELD, COMMAND_ID2);
+        commandDetailsCollection.insert(commandDetail1);
+        commandDetailsCollection.insert(commandDetail2);
+        command_detail_object_id_1 = (ObjectId)commandDetail1.get("_id");
     }
 
     @After
@@ -67,6 +71,7 @@ public class MongoRecommendationDBTest {
         userCollection.remove(new BasicDBObject());
         recommendationCollection.remove(new BasicDBObject());
         commandDetailsCollection.remove(new BasicDBObject());
+        client.close();
     }
 
     private DBCollection getCollection(String collection) {
@@ -94,7 +99,7 @@ public class MongoRecommendationDBTest {
             assertEquals(ALGORITHM_VALUE1, (Double) recommendation.get(MongoRecommendationDB.ALGORITHM_VALUE_FIELD), 0.0);
             numRecommendationsFound++;
         }
-        assertEquals(numRecommendationsFound, 1);
+        assertEquals(1, numRecommendationsFound);
     }
 
     @Test
@@ -107,7 +112,7 @@ public class MongoRecommendationDBTest {
             cursor.next();
             numRecommendationsFound++;
         }
-        assertEquals(numRecommendationsFound, 0);
+        assertEquals(0, numRecommendationsFound);
     }
 
     @Test
@@ -120,32 +125,34 @@ public class MongoRecommendationDBTest {
             cursor.next();
             numRecommendationsFound++;
         }
-        assertEquals(numRecommendationsFound, 0);
+        assertEquals(0, numRecommendationsFound);
+    }
+    
+    @Test
+    public void testSaveRecommendationEnsureUniqueness() {
+    	saveRecommendation(COMMAND_ID1, USER_ID1);
+    	saveRecommendation(COMMAND_ID2, USER_ID1);
+    	saveRecommendation(COMMAND_ID1, USER_ID1); //duplicates will not be added
+    	saveRecommendation("adf", "asdf");         //unknow cmd will not be added
+    	//duplicates will not be added
+    	recommendationDB.saveRecommendation(COMMAND_ID2, USER_ID1, "a", ALGORITHM_TYPE1, new Rationale(0));
+    	assertEquals(2, recommendationCollection.count());
     }
 
     @Test
     public void testGetAllUsers(){
-        Integer itemIndex = toolIndexMap.addItem(COMMAND_ID1);
-        prepareUsers(itemIndex);
-        assertFalse(userCollection.getCount() == 0);
+    	insertNewUsers(5);
+        assertEquals(5, recommendationDB.getAllUsers().size());
     }
     
-    private List<User> prepareUsers(Integer itemIndex) {
-        List<User> users = new ArrayList<User>();
-        for(int i=0; i < 5; i++){
-            willUpdate = new Date(System.currentTimeMillis());
-            users.add(createNewUser(i));
+    private void insertNewUsers(int amount) {
+        for(int i=0; i < amount; i++){
+            String userId =  USER_ID + i;
+            BasicDBObject new_user = new BasicDBObject()
+                    .append(MongoRecommendationDB.USER_ID_FIELD, userId)
+                    .append(MongoRecommendationDB.LAST_UPLOADED_DATE_FIELD, willUpdate);
+            userCollection.insert(new_user);
         }
-        return users;
-    }
-
-    private User createNewUser(int i) {
-        String userId =  USER_ID + i;
-        BasicDBObject new_user = new BasicDBObject()
-                .append(MongoRecommendationDB.USER_ID_FIELD, userId)
-                .append(MongoRecommendationDB.LAST_UPLOADED_DATE_FIELD, willUpdate);
-        userCollection.insert(new_user);
-        return new User(userId, willUpdate, willUpdate, new MockRecommendationDB(toolIndexMap));
     }
     
 }
